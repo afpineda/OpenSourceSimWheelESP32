f7656d1de18537ef805ac6bfdb902cff36401b88
afpineda <74291754+afpineda@users.noreply.github.com>
2024-10-04 17:44:25 +0200

    Update TelemetryIntegrationTest.ino

--- src/QualityControls/IntegrationTests/TelemetryIntegrationTest/TelemetryIntegrationTest.ino
+++ src/QualityControls/IntegrationTests/TelemetryIntegrationTest/TelemetryIntegrationTest.ino
@@ -39,13 +39,41 @@ void TelemetryDisplayMock::onTelemetryData(const telemetryData_t *data)
     }
     else
     {
+        Serial.println("*******************");
+        Serial.printf("* Frame ID: %u\n", data->frameID);
+        Serial.println("*******************");
         Serial.printf(
-            "Frame: %u Speed: %u. Pit limiter: %u. Blue flag: %u. Water temp: %u\n",
-            data->frameID,
-            data->powertrain.speed,
+            "  Gear: %c RPM: %u / %u%% SL1: %u SL2: %u Speed: %u.\n",
+            data->powertrain.gear,
+            data->powertrain.rpm,
+            data->powertrain.rpmPercent,
+            data->powertrain.shiftLight1,
+            data->powertrain.shiftLight2,
+            data->powertrain.speed);
+        Serial.printf(
+            "  Abs: %u TC: %u DRS: %u PitLim: %u LowFuel: %u ABSLevel: %u TCLevel: %u, BrakeBias: %u%%\n",
+            data->ecu.absEngaged,
+            data->ecu.tcEngaged,
+            data->ecu.drsEngaged,
             data->ecu.pitLimiter,
+            data->ecu.lowFuelAlert,
+            data->ecu.absLevel,
+            data->ecu.tcLevel,
+            data->ecu.brakeBias);
+        Serial.printf(
+            "  BlueF: %u YellowF: %u GreenF: %u Laps rem.: %u Minutes rem.: %u\n",
             data->raceControl.blueFlag,
-            data->gauges.waterTemperature);
+            data->raceControl.yellowFlag,
+            data->raceControl.greenFlag,
+            data->raceControl.remainingLaps,
+            data->raceControl.remainingMinutes);
+        Serial.printf(
+            "  WaterTemp: %u OilTemp: %u OilPressure: %f AbsFuel: %u RelFuel: %u\n",
+            data->gauges.waterTemperature,
+            data->gauges.oilTemperature,
+            data->gauges.oilPressure,
+            data->gauges.absoluteRemainingFuel,
+            data->gauges.relativeRemainingFuel);
     }
 }
 
@@ -76,7 +104,7 @@ void setup()
     Serial.begin(115200);
     Serial.println("-- READY --");
 
-    notify::begin({new TelemetryDisplayMock()}, 10,3*1024);
+    notify::begin({new TelemetryDisplayMock()}, 10, 3 * 1024);
     userSettings::cpWorkingMode = CF_CLUTCH;
     userSettings::altButtonsWorkingMode = true;
     userSettings::dpadWorkingMode = true;
