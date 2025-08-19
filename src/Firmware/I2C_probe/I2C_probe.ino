aa1c8a267d4582d4c97bed863482419131a9b65c
afpineda <74291754+afpineda@users.noreply.github.com>
2025-08-19 15:28:28 +0200

    Improve I2C probe

--- src/Firmware/I2C_probe/I2C_probe.ino
+++ src/Firmware/I2C_probe/I2C_probe.ino
@@ -9,8 +9,14 @@
  *
  */
 
-#include <HardwareSerial.h>
 #include "HAL.hpp"
+
+#if (ARDUINO_USB_MODE == 1)
+#include "USBCDC.h"
+#define Serial USBSerial
+#else
+#include <HardwareSerial.h>
+#endif
 
 //------------------------------------------------------------------
 // GLOBALS
@@ -51,15 +57,24 @@ void dump_results(std::vector<uint8_t> &addressList)
 
 void setup()
 {
-    esp_log_level_set("*", ESP_LOG_ERROR);
     Serial.begin(115200);
     Serial.println("=================================");
     Serial.println(" I2C slave device auto-discovery");
     Serial.println("=================================");
     Serial.println("");
 
-    Serial.printf("SDA = #%d. SCL = #%d. Please, wait ...\n\n", SDA, SCL);
+#if defined(SECONDARY_SDA) && defined(SECONDARY_SCL)
+    internals::hal::i2c::initialize(
+        SECONDARY_SDA,
+        SECONDARY_SCL,
+        I2CBus::SECONDARY);
+#endif
+}
+
+void loop()
+{
     std::vector<uint8_t> addressList;
+    Serial.printf("SDA = #%d. SCL = #%d. Please, wait ...\n\n", SDA, SCL);
     internals::hal::i2c::probe(addressList);
     dump_results(addressList);
     Serial.println("");
@@ -67,20 +82,15 @@ void setup()
 
 #if defined(SECONDARY_SDA) && defined(SECONDARY_SCL)
     Serial.printf("SDA = #%d. SCL = #%d. Please, wait ...\n\n", SECONDARY_SDA, SECONDARY_SCL);
-    internals::hal::i2c::initialize(
-        SECONDARY_SDA,
-        SECONDARY_SCL,
-        I2CBus::SECONDARY);
     addressList.clear();
-    internals::hal::i2c::probe(addressList, true);
+    internals::hal::i2c::probe(addressList, I2CBus::SECONDARY);
     dump_results(addressList);
     Serial.println("");
     Serial.println("");
 #endif
 
-    Serial.println("Done. Reset to repeat auto-discovery.");
-}
-
-void loop()
-{
+    Serial.println("Done. Repeating autodiscovery in 30 seconds...");
+    Serial.println("");
+    Serial.println("");
+    vTaskDelay(pdMS_TO_TICKS(30 * 1000));
 }
